4efe6793096f1ebae59b416dc8f34b6e099cbff3
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-21 16:57:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,11 @@ def key_for_min_value(hash)
     if smallest_value == 0 || value < smallest_value
     # conditional evaluates whether the value passed in is less than the value it is being compared to
       smallest_value = value
+      # sets passed in value equal to smallest_value variable
       new_hash = name
+      # adds key to new_hash variable
     end
   end
   new_hash
+  # updates return value of #key_for_min_value to the desired key
 end

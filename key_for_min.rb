a91fc3b4efab8ae9c4f275555c00840b314c8eb1
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-21 16:09:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
+ # if hash != {}
+#    groups = hash.group_by do |n, value| value <=> hash.first 
+#      less_than    = groups[-1] || []
+#      first        = groups[0]  || []
+#      greater_than = groups[1]  || []
+#    end
+#    key_for_min_value(less_than) + first + key_for_min_value(greater_than)
+#  end  
+# key_for_min_value(less_than)
+#end
 
+  lowest = hash.inject do |first, second|
+    first < second ? first : second
+  end
+  lowest
+#longest = %w{ cat sheep bear }.inject do |memo, word|
+#   memo.length > word.length ? memo : word
 end

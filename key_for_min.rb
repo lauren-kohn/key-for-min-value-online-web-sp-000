a0aba344459207afdec3bc6e368ef5343ded3e85
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-21 16:42:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -22,7 +22,7 @@ def key_for_min_value(hash)
   
   smallest_value = 0
   lowest = hash.delete_if do |name, value|
-    if value < smallest_value
+    if smallest_value == value || value < smallest_value
       value = smallest_value
     end
   end
